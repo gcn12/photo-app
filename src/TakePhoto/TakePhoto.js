19caa0d6a3f8eebbd571a282f57c4be8d766b02d
Gareth Ng <garethcng@gmail.com>
2020-11-05 10:00:07 -0500

    update

--- src/TakePhoto/TakePhoto.js
+++ src/TakePhoto/TakePhoto.js
@@ -4,12 +4,12 @@ import './TakePhoto.css'
 const constraints = {
     video: {
       width: {
-        min: 1280,
+        // min: 1280,
         ideal: 1920,
         max: 2560,
       },
       height: {
-        min: 720,
+        // min: 720,
         ideal: 1080,
         max: 1440,
       },
