03d249b0765693dcafaca49dc2422ff5e82963f9
Gareth Ng <garethcng@gmail.com>
2020-11-05 10:22:50 -0500

    update

--- src/TakePhoto/TakePhoto.js
+++ src/TakePhoto/TakePhoto.js
@@ -3,16 +3,16 @@ import './TakePhoto.css'
 
 const constraints = {
     video: {
-      width: {
-        // min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        // min: 720,
-        ideal: 1080,
-        max: 1440,
-      },
+    //   width: {
+    //     // min: 1280,
+    //     ideal: 1920,
+    //     max: 2560,
+    //   },
+    //   height: {
+    //     // min: 720,
+    //     ideal: 1080,
+    //     max: 1440,
+    //   },
       facingMode: 'environment',
     },
 };
@@ -38,7 +38,7 @@ const video = document.querySelector("#video");
 
 async function initializeCamera() {
     // stopVideoStream();
-    // constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
 
     try {
       videoStream = await navigator.mediaDevices.getUserMedia(constraints);
