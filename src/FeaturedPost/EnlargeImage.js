a914919975584af99a401b212598474064554e10
Gareth Ng <garethcng@gmail.com>
2021-01-11 20:27:57 -0500

    updates

--- src/FeaturedPost/EnlargeImage.js
+++ src/FeaturedPost/EnlargeImage.js
@@ -20,9 +20,9 @@ const EnlargeImage = (props) => {
             }
             <Close onClick={props.closeImage}>&times;</Close>
             {/* <Placeholder opacity={showPlaceholder ? 0 : 1}></Placeholder> */}
-            <Image src={props.image} onLoad={()=>setShowPlaceholder(true)} opacity={showPlaceholder ? 1 : 0} scale={showPlaceholder ? 'scale(1)' : 'scale(.75)'}>
-                
-            </Image>
+            <div style={{maxHeight: '92vw', maxWidth: '92vw'}}>
+                <Image src={props.image} onLoad={()=>setShowPlaceholder(true)} opacity={showPlaceholder ? 1 : 0} scale={showPlaceholder ? 'scale(1)' : 'scale(.75)'} />
+            </div>
         </Container>
     )
 }
